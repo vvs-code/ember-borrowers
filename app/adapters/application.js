e8bd3998fe65325bde777532179f285bf7857af9
Vasil Vanchuk <vasil.vanchuk@strevus.com>
2015-02-26 16:55:48 +0300

    Retrieve / display articles

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-  namespace: 'api' // http://api.ember-cli-101.com
+  namespace: 'api/v2' // http://api.ember-cli-101.com
 });
--- app/router.js
+++ app/router.js
@@ -9,15 +9,14 @@ Router.map(function() {
   this.resource('friends', function() {
     this.route('new');
 
-    this.route('show', {
-      path: ':friend_id'
+    this.route('show', { path: ':friend_id' }, function() {
+      this.resource('articles', function() {});
     });
 
     this.route('edit', {
       path: ':friend_id/edit'
     });
   });
-  this.resource('articles', function() {});
 });
 
 export default Router;
