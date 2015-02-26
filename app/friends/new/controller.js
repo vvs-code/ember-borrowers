f7e268281ee7762becdb9acc203d597de0748e40
Vasil Vanchuk <vasil.vanchuk@strevus.com>
2015-02-26 14:43:50 +0300

    Create friends.base controller

--- app/friends/new/controller.js
+++ app/friends/new/controller.js
@@ -1,31 +1,7 @@
-import Ember from 'ember';
+import FriendsBaseController from '../base/controller.js';
 
-export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.email',
-    'model.firstName',
-    'model.lastName',
-    'model.twitter',
-    function() {
-      return !Ember.isEmpty(this.get('model.email')) &&
-        !Ember.isEmpty(this.get('model.firstName')) &&
-        !Ember.isEmpty(this.get('model.lastName')) &&
-        !Ember.isEmpty(this.get('model.twitter'));
-    }
-  ),
+export default FriendsBaseController.extend({
   actions: {
-    save: function() {
-      if(this.get('isValid')) {
-        var _this = this;
-        this.get('model').save().then(function(friend) {
-          _this.transitionToRoute('friends.show', friend);
-        });
-      } else {
-        this.set('errorMessage', 'You have to fill all the fields');
-      }
-
-      return false;
-    },
     cancel: function() {
       this.transitionToRoute('friends');
 
