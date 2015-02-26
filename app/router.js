ce9a1ab70a7ed22c6bbf590ef6d031f395a1f984
Vasil Vanchuk <vasil.vanchuk@strevus.com>
2015-02-26 12:58:12 +0300

    Move templates to pods

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('friends', function() {});
+  this.resource('friends', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
